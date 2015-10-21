c78f9d6eb5af1ec23cf14a706d776a64ea90325f
ValisStigma <rafael_krucker@hotmail.com>
2015-10-21 15:56:02 +0200

    init

---
+++ sources/classes/controllers/EventListController.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rafael on 21.10.2015.
+ */
+
+
+var myApp = angular.module('lafete',[]);
+
+myApp.controller('EventListController', ['$scope', function($scope) {
+    $scope.events = [
+        { name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z') },
+        { name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z') },
+        { name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z') }
+    ];
+}]);
+
+

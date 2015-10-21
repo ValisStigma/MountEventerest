ec3f6c343d5b0fbc4e1dc184e72297384c0fc217
ValisStigma <rafael_krucker@hotmail.com>
2015-10-21 16:12:21 +0200

    Added BaseController

--- sources/classes/controllers/EventListController.js
+++ sources/classes/controllers/EventListController.js
@@ -2,15 +2,13 @@
  * Created by rafael on 21.10.2015.
  */
 
-
-var myApp = angular.module('lafete',[]);
-
-myApp.controller('EventListController', ['$scope', function($scope) {
-    $scope.events = [
-        { name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z') },
-        { name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z') },
-        { name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z') }
-    ];
-}]);
-
+define([], function () {
+    return function($scope) {
+        $scope.events = [
+            { name: 'Lunch', place: 'Rapperswil', date: new Date('2015-10-10T10:00:00.000Z') },
+            { name: 'Dinner', place: 'Zürich', date: new Date('2015-04-05T16:00:00.000Z') },
+            { name: 'Dinner', place: 'Rapperswil', date: new Date('2015-12-08T17:00:00.000Z') }
+        ];
+    }
+});
 
--- sources/classes/modules/lafete.js
+++ sources/classes/modules/lafete.js
@@ -1,9 +1,10 @@
 // declare dependency to angular (similar to import in java)
-define(['frameworks/angular'], function (Angular) {
+define(['frameworks/angular', 'app/controllers/EventListController'], function (Angular, EventListController) {
 
     // Create new empty app/module named 'lafete'
     var Lafete = Angular.module('lafete', []);
-
+    EventListController.$inject = ['$scope'];
+    Lafete.controller('EventListController', EventListController);
     // export module to use it in other classes
     return Lafete;
 });

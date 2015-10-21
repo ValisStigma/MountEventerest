aba3ea91825325951c7368d3ba3de1a2b70b5213
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-10-21 16:49:22 +0200

    ADD: event class

---
+++ sources/model/Event.js
@@ -0,0 +1,10 @@
+var Event = function(id, name, description, targetGroup, contributionsDescription, location, times, maximalAmountOfGuests, guests){
+
+};
+
+var Location = function(name, street, zipCode, city){
+
+};
+var Guest = function(id, name, contribution, comment, canceled){
+
+};

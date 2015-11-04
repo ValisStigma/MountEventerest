4809c9913689759a063b72660c0c54d8a422efd8
Pascal <p-marty@sunrise.ch>
2015-11-04 16:09:01 +0100

    MOD: events mit ng repeat

--- sources/classes/controllers/EventListController.js
+++ sources/classes/controllers/EventListController.js
@@ -2,7 +2,7 @@ define([], function () {
     return function($scope, $http) {
         var response = $http.get("http://localhost:8080/api/events");
         response.success(function(data, status, headers, config){
-           $scope.events = data;
+           $scope.events = data.events;
         });
         response.error(function(data, status, headers, config){
             $scope.events = "Could not load events";

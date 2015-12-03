f91da4b62c68e32a4f26cbda3361ca920342adf9
Pascal <p-marty@sunrise.ch>
2015-12-04 00:03:28 +0100

    ADD: MaxAmountGuests, Style fixes

--- sources/classes/controllers/EventDetailController.js
+++ sources/classes/controllers/EventDetailController.js
@@ -1,5 +1,5 @@
 define([], function () {
-    var EventDetailController = function($scope, eventRepository, $routeParams){
+    var EventDetailController = function($scope, eventRepository, $routeParams, $window){
         var event_id = $routeParams.id;
         eventRepository.get(event_id, function(data){
             $scope.event = data;
@@ -13,13 +13,18 @@ define([], function () {
         });
         $scope.addGuest = function(){
             var guest = $scope.guest;
+            var amountOfGuests = $scope.guests.length;
             var event = $scope.event;
-            eventRepository.addGuest(event, guest, function(data){
-                $scope.event.guests.push(data);
-            }, function(){
-                $scope.error = "Error";
-            });
-        }
+            if(event.maximalAmountOfGuests >= amountOfGuests){
+                eventRepository.addGuest(event, guest, function(data){
+                    $scope.event.guests.push(data);
+                }, function(){
+                    $scope.error = "Error";
+                });
+            } else {
+                $window.alert("Event is full");
+            }
+        };
         $scope.editGuest = function(){
             var event = $scope.event;
             eventRepository.edit(event.id, function(data){
@@ -29,7 +34,7 @@ define([], function () {
             });
         }
     };
-    EventDetailController.$inject = ['$scope', 'eventRepository', '$routeParams'];
+    EventDetailController.$inject = ['$scope', 'eventRepository', '$routeParams', 'window'];
     return EventDetailController;
 });
 
